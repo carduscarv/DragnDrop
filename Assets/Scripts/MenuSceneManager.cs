f5ed666e9e9d9a3fdb896c478282d0c4eea201d1
mong <mong@agate.id>
2020-07-03 18:58:47 +0700

    first init

---
+++ Assets/Scripts/MenuSceneManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuSceneManager : MonoBehaviour
+{
+    public void GoToScene(string scene){
+
+        SceneManager.LoadScene(scene);
+    }
+
+    public void Quit(){
+        Application.Quit();
+    }
+}
